00942d7b603ab9059da18aedabc4f462e8bae5f3
lihanxi <3138220743@qq.com>
2022-07-08 12:29:36 +0800

    2022070801

--- app/src/pages/Home/index.vue
+++ app/src/pages/Home/index.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
-    <!-- 全局组件直接引用 -->
+    <!-- 全局组件直接使用，无须引用 -->
     <TypeNav/>
+    <ListContainer/>
   </div>
 </template>
 
 <script>
+// 引用其他组件
+import ListContainer from '@/pages/Home/ListContainer'
 export default {
-name:'index'
+name:'index',
+components:{
+  ListContainer
+}
 }
 </script>
 
