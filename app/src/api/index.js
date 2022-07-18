2f763ec8accf480082ff0d66701e7f41306ce5c5
lihanxi <3138220743@qq.com>
2022-07-18 20:58:38 +0800

    20220718

--- app/src/api/index.js
+++ app/src/api/index.js
@@ -8,5 +8,5 @@ export const reqCategoryList = () => {
   //箭头函数可以在程序任意地方使用,箭头函数返回即为服务器的数据
   //下面箭头函数返回值：返回的是什么? promise,即为返回服务器的数据
   //return关键字，千万别忘记书写，如果忘记书写，你在任意地方获取的都是undeinfed
-  return requests({ method: 'get', url: '/product/getBaseCategoryList', method: 'get', });
+  return requests({ method: 'get', url: '/product/getBaseCategoryList', method: 'get'});
 }
